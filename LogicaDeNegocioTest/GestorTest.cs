28844257ac4eb6ace5a8083d903af9e9610f4c7e
Bryan Smith <brperez@ulasallista.edu.co>
2020-05-17 11:41:00 -0500

    Inicio del proyecto

---
+++ LogicaDeNegocioTest/GestorTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogicaDeNegocioTest
+{
+    [TestClass]
+    public class GestorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
